402bec8ebf5190d6b9a9a832cb0d38b79ea39c74
System <none>
2017-09-29 00:39:11 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var express = require('express');
 var app = express;
 
 var server = app.createServer(function(req,res) {
-  res.
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  res.write(req.url);
 });
 
 server.listen(5000);
