cefd25278d04f884f2493de80b7955c1ee0089f2
System <none>
2017-09-29 00:50:11 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 "use strict";
 
+var http = require('http');
 var express = require('express');
-
 var app = express;
 
-var server = app.createServer(function(req,res) {
+var server = http.createServer(function(req,res) {
   res.writeHead(200, { 'Content-type': 'application/json' });
   res.write(req.url);
+  res.end('\nFinished Processing');
 });
 
-server.listen(5000);
+server.listen(8080);
