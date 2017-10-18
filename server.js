7cb11fade33aa279cd92ddfa60c2ed175890f3b1
Glitch (timestamp-fcc-api-project) <none>
2017-10-18 02:09:41 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,15 +2,28 @@
 
 var http = require('http');
 var URL = require('url');
-var express = require('express');
-var app = express;
+
+var monthnames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 var server = http.createServer(function(req,res) {
   res.writeHead(200, { 'Content-type': 'application/json' });
   var mypathname = URL.parse(req.url).pathname;
-  res.write(mypathname.slice(1));
   
-  res.end('\nFinished Processing');
+  var daterequest = decodeURIComponent(mypathname.slice(1));
+  
+  var myDate = new Date(daterequest);
+  
+  // if (myDate.getMonth().toString() !== "NaN") {
+  if (!!myDate) {
+    var naturaldateformat = monthnames[myDate.getMonth()] + " " + myDate.getDate() + ", " + myDate.getFullYear();
+    var z = JSON.stringify({"unix": myDate.getTime(), "natural": naturaldateformat});
+    res.write(z);
+  } else {
+    
+    res.write(JSON.stringify({"unix": "null", "natural": "null"}));
+  }
+  res.end();
 });
 
 server.listen(8080);
