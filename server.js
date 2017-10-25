e9cb8956c793bcfa75d9284f741e9375c72ca68e
Glitch (timestamp-fcc-api-project) <none>
2017-10-25 00:40:17 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,40 +12,44 @@ var server = http.createServer(function(req,res) {
   var baseJSON = JSON.stringify({"unix": "null", "natural": "null"});
   
   var daterequest = decodeURIComponent(mypathname.slice(1));
-  var isZero = Object.keys(daterequest).length === 0;
- 
-  var stringDate = String(daterequest);
-  res.write("My stringDate "+ stringDate + " => " + new Date(stringDate) + "\n");
-  var numrequest = Number(daterequest);
-  res.write("My numrequest "+ numrequest + " => " + new Date(numrequest) + "\n");
-  var caseResult;
   
-  switch(numrequest === NaN) {
-    case true:
-        caseResult = stringDate;
-      res.write("The case was True");
-        break;
-    case false:
-        caseResult = numrequest;
-      res.write("The case was False");
-        break;
-    default:
-        caseResult = "December 25, 0000";
-  }
-   
-  var myDate = new Date(caseResult);
-  // res.write("this is my date "+ myDate);
-   if (isZero) {
-    res.end(baseJSON);
-  } else if (myDate == "Invalid Date") {
-    res.write("My date said it was Invalid Date =>" + myDate +"\n" );
-    res.end(baseJSON);
-  } else if (caseResult != true) {  
-      var naturaldateformat = monthnames[myDate.getMonth()] + " " + myDate.getDate() + ", " + myDate.getFullYear();
-      var z = JSON.stringify({"unix": myDate.getTime(), "natural": naturaldateformat});
-      res.end(z);
+  var isZero = Object.keys(daterequest).length === 0;
+    if (!isZero) {
+      var stringDate = String(daterequest);
+      // res.write("My stringDate "+ stringDate + " => " + new Date(stringDate) + "\n");
+      var numrequest = Number(daterequest);
+      // res.write("My numrequest "+ numrequest + " => " + new Date(numrequest) + "\n" + (isNaN(numrequest)) + "\t\n");
+      var caseResult;
+
+      switch (isNaN(numrequest)) {
+        case true:
+            caseResult = stringDate;
+            // res.write("The case was True => " + caseResult + "\n");
+            break;
+        case false:
+            caseResult = numrequest;
+          // res.write("The case was False => " + caseResult + "\n");
+            break;
+        default:
+            caseResult = "December 25, 0000";
+      }
+
+      var myDate = new Date(caseResult);
+      res.write("this is my date "+ myDate + " => " + (myDate !== "Invalid Date"));
+    if (myDate !== "Invalid Date") {
+        res.write("My date said it was not Invalid Date =>" + myDate +"\n" );
+        if (caseResult) {  
+          var naturaldateformat = monthnames[myDate.getMonth()] + " " + myDate.getDate() + ", " + myDate.getFullYear();
+          var z = JSON.stringify({"unix": myDate.getTime(), "natural": naturaldateformat});
+          res.end(z);
+        } else {
+          res.end("Something is wrong here");
+        }
+    } else {
+      res.end(baseJSON);
+    }
   } else {
-    res.end("Something is wrong here");
+      res.end(baseJSON);
   }
 });
 
